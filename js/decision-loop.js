d20e81737fafa6deb218f34a59e071fa83201789
citlallimontiel <cmontiel8407@mysvc.skagit.edu>
2024-01-23 13:10:34 -0800

    decision loop added

---
+++ js/decision-loop.js
@@ -0,0 +1,7 @@
+// Array of book titles
+const bookTitles = ["Ghost Dog Secrets", "Number the Stars", "The Old Willis Place", "Amulet Book One: The Stonekeeper"];
+
+// For loop to iterate through the book titles
+for (let i = 0; i < bookTitles.length; i++) {
+	console.log(`Book ${i + 1}: ${bookTitles[i]}`);
+}
