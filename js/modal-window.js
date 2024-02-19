1c06396180fe7276023a4cbc04b4167e4de60bf3
citlallimontiel <cmontiel8407@mysvc.skagit.edu>
2024-02-18 16:29:47 -0800

    corrected line 5

--- js/modal-window.js
+++ js/modal-window.js
@@ -2,7 +2,7 @@ var modal = (function() {
 	var $window = $(window);
 	var $modal = $('<div class="modal"/>');
 	var $content = $('<div class="modal-content"/>');
-	var $close = $('<button role="button" class="modal-close">close</button');
+	var $close = $('<button role="button" class="modal-close">close</button>');
 	
 	$modal.append($content, $close);
 	
